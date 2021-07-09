da5cf58f1d79a882f65e770f202659f20de42dee
JunierCruz <85705209+JunierCruz@users.noreply.github.com>
2021-07-09 06:23:46 -0500

    Add files via upload

---
+++ tarea8.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+int main (){
+ 	int numero;
+ 	
+ 
+ 		cout<<"Digite un numero: "; cin>>numero;
+ 		
+	
+	 for(int i=1;i<=2000000;i++){
+	 	cout<<numero<<" * "<<i<<" = "<<numero*i<<endl;
+}
+ 
+//out<<"\n\n";
+//ystem("pause");
+ 
+ return 0;
+}
